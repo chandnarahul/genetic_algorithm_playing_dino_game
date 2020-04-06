db6c5dbf8791d2e4e018cfa9a1641de914c50838
Rahul Chandna <chandnarahul@yahoo.com>
2020-04-06 23:26:51 +0200

    bird as cactus fix

--- src/main/java/dino/geneticalgorithm/sensor/ImageSensor.java
+++ src/main/java/dino/geneticalgorithm/sensor/ImageSensor.java
@@ -25,7 +25,7 @@ public class ImageSensor {
                 up = true;
                 objectPixel = i;
             }
-            for (int yAxisBottomUp = image.getHeight() - 1; yAxisBottomUp > image.getHeight() - 15; yAxisBottomUp--) {
+            for (int yAxisBottomUp = image.getHeight() - 1; yAxisBottomUp > image.getHeight() - 12; yAxisBottomUp--) {
                 if (isGrayPixel(i, yAxisBottomUp)) {
                     bottom = true;
                     objectPixel = i;
