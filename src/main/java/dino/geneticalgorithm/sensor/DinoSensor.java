257ca79bd07375bcb0136d2dacf473b83ef8399e
Rahul Chandna <chandnarahul@yahoo.com>
2020-04-08 20:49:31 +0200

    moved image sensor logic to dino sensor class as there is only image processing required in this project

--- src/main/java/dino/geneticalgorithm/sensor/DinoSensor.java
+++ src/main/java/dino/geneticalgorithm/sensor/DinoSensor.java
@@ -1,22 +1,29 @@
 package dino.geneticalgorithm.sensor;
 
+import dino.DinoConstants;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.io.File;
 
 public class DinoSensor {
-    private double speed;
     private final int DINO_X_AXIS = 78;
     private final int DINO_Y_AXIS = 154;
-    private final ImageSensor imageSensor;
+    private boolean isObjectFlying = Boolean.FALSE;
+    private boolean isObjectCloserToTheGround = Boolean.FALSE;
+    private int distanceFromFirstObjectXAxis = 0;
+    private int groundObjectWidth = 0;
+    private final BufferedImage image;
 
-    protected DinoSensor(BufferedImage bufferedImage) {
-        this.speed = 0;
-        if (bufferedImage.getWidth() > 250) {
-            this.imageSensor = new ImageSensor(removeDinoFloorAndSkyFromImage(bufferedImage));
+    protected DinoSensor(BufferedImage image) {
+        int gameCanvasWidth = 500;
+        if (image.getWidth() == gameCanvasWidth) {
+            this.image = removeDinoFloorAndSkyFromImage(image);
         } else {
-            this.imageSensor = new ImageSensor(bufferedImage);
+            this.image = image;
         }
-        this.imageSensor.findObjects();
+        this.findObject();
     }
 
     private BufferedImage removeDinoFloorAndSkyFromImage(BufferedImage image) {
@@ -24,35 +31,110 @@ public class DinoSensor {
     }
 
     public DataBufferByte imageDataBuffer() {
-        return (DataBufferByte) image().getRaster().getDataBuffer();
+        return (DataBufferByte) image.getRaster().getDataBuffer();
+    }
+
+
+    private boolean isAnyPixelFoundAtBottom(int firstPixelFoundAt) {
+        int fewPixelsTowardsRight = 10;
+        int pixelsFound = 0;
+        for (int traverseXAxis = firstPixelFoundAt; traverseXAxis < image.getWidth(); traverseXAxis++) {
+            if (isGrayPixel(traverseXAxis, image.getHeight() - 1)) {
+                return Boolean.TRUE;
+            } else {
+                pixelsFound++;
+                if (pixelsFound == fewPixelsTowardsRight) {
+                    return Boolean.FALSE;
+                }
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    private void findObject() {
+        for (int i = 0; i < image.getWidth(); i++) {
+            if (isAnyPixelFoundAtTop(i)) {
+                distanceFromFirstObjectXAxis = i;
+                this.isObjectFlying = Boolean.TRUE;
+                this.isObjectCloserToTheGround = Boolean.FALSE;
+            }
+            if (isAnyPixelFoundAtBottom(i)) {
+                this.isObjectCloserToTheGround = Boolean.TRUE;
+                this.isObjectFlying = Boolean.FALSE;
+            }
+            if (this.isObjectCloserToTheGround || this.isObjectFlying) {
+                distanceFromFirstObjectXAxis = i;
+                break;
+            }
+        }
+        if (this.isObjectCloserToTheGround) {
+            this.groundObjectWidth = determineWidthOfTheGroundObject();
+        }
+    }
+
+    private boolean isAnyPixelFoundAtTop(int i) {
+        return isGrayPixel(i, 0);
+    }
+
+    private int determineWidthOfTheGroundObject() {
+        int pixelNotFound = 0;
+        int lastPixelFound = 0;
+        for (int i = distanceFromFirstObjectXAxis; i < image().getWidth(); i++) {
+            if (isGrayPixel(i, image().getHeight() - 1)) {
+                lastPixelFound = i;
+                pixelNotFound = 0;
+            } else {
+                pixelNotFound++;
+            }
+            if (pixelNotFound > 10) {
+                return calculateObjectWidth(lastPixelFound);
+            }
+        }
+        return calculateObjectWidth(lastPixelFound);
+    }
+
+    private int calculateObjectWidth(int lastPixelFound) {
+        extractObjectAsImage(lastPixelFound);
+        return lastPixelFound - distanceFromFirstObjectXAxis;
+    }
+
+    private void extractObjectAsImage(int lastPixelFound) {
+        try {
+            if (DinoConstants.IN_DEBUG_MODE && lastPixelFound != 0) {
+                ImageIO.write(image.getSubimage(distanceFromFirstObjectXAxis, 0, lastPixelFound - distanceFromFirstObjectXAxis, 30), "png", new File("images/block.png"));
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private boolean isGrayPixel(int xAxis, int yAxisBottomUp) {
+        final int grayScale = 90;
+        int pixel = image.getRGB(xAxis, yAxisBottomUp) & 0xFF;
+        return pixel < grayScale;
     }
 
     public BufferedImage image() {
-        return imageSensor.getImage();
+        return image;
     }
 
     public boolean isObjectFlying() {
-        return imageSensor.isObjectFlying();
+        return isObjectFlying;
     }
 
     public boolean isObjectCloserToTheGround() {
-        return imageSensor.isObjectCloserToTheGround();
-    }
-
-    public boolean isAnyObjectFound() {
-        return imageSensor.isAnyObjectFound();
+        return isObjectCloserToTheGround;
     }
 
     public int distanceFromObject() {
-        return imageSensor.getDistanceFromObject();
+        return distanceFromFirstObjectXAxis;
     }
 
-
-    public double speed() {
-        return speed;
+    public boolean isAnyObjectFound() {
+        return isObjectCloserToTheGround || isObjectFlying;
     }
 
-    public void setSpeed(double speed) {
-        this.speed = speed;
+    public int groundObjectWidth() {
+        return groundObjectWidth;
     }
 }
