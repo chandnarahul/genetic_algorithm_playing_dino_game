063a6f089ca2fc17356a4186a0b89ab268592a00
Rahul Chandna <chandnarahul@yahoo.com>
2020-04-12 22:02:29 +0200

    refactored: increased population size

--- src/main/java/dino/geneticalgorithm/simple_genetic_algorithm/SimpleGAConstants.java
+++ src/main/java/dino/geneticalgorithm/simple_genetic_algorithm/SimpleGAConstants.java
@@ -2,7 +2,7 @@ package dino.geneticalgorithm.simple_genetic_algorithm;
 
 public class SimpleGAConstants {
     public static final int CHROMOSOME_LENGTH = 8;
-    public static final int POPULATION_SIZE = 5;
+    public static final int POPULATION_SIZE = 10;
     public static final double MUTATION_RATE = 0.001;
     public static final double CROSSOVER_RATE = 0.95;
     public static final double NUMBER_OF_ITERATIONS = 2;
