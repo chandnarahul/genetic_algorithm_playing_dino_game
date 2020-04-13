0d2097e66356d19026b0ae36f139f524d0037248
Rahul Chandna <chandnarahul@yahoo.com>
2020-04-13 14:15:48 +0200

    refactored: fixed failing test and removed apache commons dependency since it is not used

--- src/test/java/dino/geneticalgorithm/sensor/DinoImageSensorInteractionTest.java
+++ src/test/java/dino/geneticalgorithm/sensor/DinoImageSensorInteractionTest.java
@@ -10,8 +10,9 @@ public class DinoImageSensorInteractionTest {
 
     @Test(expected = RuntimeException.class)
     public void should_identify_that_game_is_over() throws IOException {
+        ArrayList<DinoSensor> imageBuffers = new ArrayList<>();
         for (int i = 0; i < DinoImageSensorInteraction.MAX_COMMON_IMAGES; i++) {
-            new DinoImageSensorInteraction(ImageIO.read(DinoSensorTest.class.getResourceAsStream("/game_over.png")), new ArrayList<>());
+            new DinoImageSensorInteraction(ImageIO.read(DinoSensorTest.class.getResourceAsStream("/game_over.png")), imageBuffers);
         }
     }
 }
